6c78429de63e891b4c6281d6f91302b250645e3e
nabbisen <heddi.nabbisen@gmail.com>
2019-04-04 18:54:40 +0900

    update consts

--- bh_tcp_client.py
+++ bh_tcp_client.py
@@ -1,12 +1,13 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "127.0.0.1"
+target_port = 9999
+send_data = b"GET /?param=test HTTP/1.1\r\nHost: localhost\r\n\r\n"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
-client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
+client.send(send_data)
 res = client.recv(4096)
 
 print(res)
