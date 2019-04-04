4abec90a28926050f47fec7a02c99f0ec844221f
nabbisen <heddi.nabbisen@gmail.com>
2019-04-04 18:18:43 +0900

    rename vars

--- bh_tcp_client.py
+++ bh_tcp_client.py
@@ -7,6 +7,6 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
 client.send(b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n")
-response = client.recv(4096)
+res = client.recv(4096)
 
-print(response)
+print(res)
